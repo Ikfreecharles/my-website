816f6d9f21fd8daf074dfa58b9ca34f9d0f86219
Charles Ikulayo <sleektrends7@gmail.com>
2025-12-31 17:24:53 +0100

    fix: IC: update CNAME

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://charlesikulayo.dev",
+  url: "https://www.charlesikulayo.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
