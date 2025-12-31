30d45e4c099749a917081e322b6625a86df230be
Charles Ikulayo <sleektrends7@gmail.com>
2025-12-31 19:03:36 +0100

    fix: IC: remove CNAME file

---
+++ state-management-design/state-management-design.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+import Heading from "@theme/Heading";
+import { PageWrapper } from "@site/src/components/Layout/layout";
+
+export default function StateManagementDesign(): ReactNode {
+  return (
+    <PageWrapper>
+      <Heading as="h1">Page is under construction</Heading>
+    </PageWrapper>
+  );
+}
